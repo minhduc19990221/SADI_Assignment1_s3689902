20cb8dc59fe57238c9952a9ac374558adea11e48
nguyencaominhduc <s3689902@rmit.edu.vn>
2021-04-03 20:58:08 +0700

    instantialize the JUnit test, prepare for testing.

---
+++ test/student/enroll/management/CourseTest.java
@@ -0,0 +1,40 @@
+package student.enroll.management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void printStudents() {
+    }
+
+    @Test
+    void getCourseId() {
+    }
+
+    @Test
+    void setCourseId() {
+    }
+
+    @Test
+    void getCourseName() {
+    }
+
+    @Test
+    void setCourseName() {
+    }
+
+    @Test
+    void getCredits() {
+    }
+
+    @Test
+    void setCredits() {
+    }
+}
---
+++ test/student/enroll/management/StudentTest.java
@@ -0,0 +1,40 @@
+package student.enroll.management;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void addCourse() {
+    }
+
+    @Test
+    void removeCourse() {
+    }
+
+    @Test
+    void getStudentId() {
+    }
+
+    @Test
+    void setStudentId() {
+    }
+
+    @Test
+    void getStudentName() {
+    }
+
+    @Test
+    void setStudentName() {
+    }
+
+    @Test
+    void getBirthDate() {
+    }
+
+    @Test
+    void setBirthDate() {
+    }
+}
